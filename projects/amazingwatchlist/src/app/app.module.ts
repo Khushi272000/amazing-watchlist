e5ed17a7b52a9f5a0752136dd4a192078e5a1958
Khushi272000 <khushipatel272000@gmail.com>
2021-03-09 17:37:52 -0500

    added search result and serch details

--- projects/amazingwatchlist/src/app/app.module.ts
+++ projects/amazingwatchlist/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchComponent } from './search/search.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
-import { ShowDetailsComponent } from './show-details/show-details.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
+import { ShowDetailsComponent } from './show-details/show-details.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule} from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -25,19 +26,19 @@ import { MatCardModule } from '@angular/material/card';
     SearchComponent,
     SearchResultsComponent,
     ShowInfoComponent,
-    ShowDetailsComponent,
-    WatchlistComponent
+    WatchlistComponent,
+    ShowDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatInputModule,
-    FormsModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    FormsModule,
+    MatCardModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/amazingwatchlist/src/app/show-details/show-details.component.ts
+++ projects/amazingwatchlist/src/app/show-details/show-details.component.ts
@@ -1,13 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {Show} from '../model/show.model';
 
 @Component({
-  selector: 'app-show-details',
-  templateUrl: './show-details.component.html',
-  styleUrls: ['./show-details.component.css']
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.css']
 })
-export class ShowDetailsComponent implements OnInit {
-
-  constructor() { }
+export class SearchResultsComponent implements OnInit {
+  shows :Array<Show>;
+  constructor() {
+    this.shows = [new Show({
+      name: 'Doctor Who',
+      language: 'English',
+      genres: ['Sci-Fi', 'Action', 'Adventure'],
+      id: 210,
+      summary: 'A show about a time travelling alien called The Doctor.',
+      status: 'ongoing',
+      image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
+    }),
+      new Show({
+        name: 'The magicians',
+        language: 'English',
+        genres: ['Sci-Fi', 'Action', 'Adventure'],
+        id: 210,
+        summary: 'A show about magicians and their trauma.',
+        status: 'ongoing',
+        image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
+      }),
+      new Show({
+        name: 'Smallville',
+        language: 'English',
+        genres: ['Sci-Fi', 'Action', 'Adventure'],
+        id: 210,
+        summary: 'A show about Superman as a teenager.',
+        status: 'ongoing',
+        image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
+      })
+    ];
+  }
 
   ngOnInit(): void {
   }
